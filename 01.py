883cf41db39cc1972eb3f7fa34bbbfe4e05bea31
henrique nc <luizh38@hotmail.com>
2023-11-20 23:07:12 -0300

    POO

--- 01.py
+++ 01.py
@@ -1,14 +1,27 @@
 import pandas as pd
-import os
 
-diretorio_atual = os.path.dirname(os.path.abspath(__file__))
-caminho_planilha = os.path.join(diretorio_atual, 'prof.xlsx')
-# Carregue a planilha original
-planilha_original = pd.read_excel(caminho_planilha)
+professores_lotados = pd.DataFrame(columns=['URE', 'USE', 'MUNICIPIO', 'COD_ESCOLA', 'COD_MEC','COD_SETOR', 'ESCOLA_MATRICULA', 'VINCULO', 'SERVIDOR','COD_DISCIPLINA', 'DISCIPLINA'])
 
-# Filtrar as linhas com base em uma condição
-condicao_filtro = planilha_original[' COD_DISCPLINA'] == 2002
-linhas_filtradas = planilha_original[condicao_filtro]
+# Suponha que você tenha um objeto com atributos correspondentes aos nomes das colunas
+class Objeto:
+    def __init__(self, URE, USE, MUNICIPIO, COD_ESCOLA, COD_MEC, COD_SETOR, ESCOLA_MATRICULA, VINCULO, SERVIDOR, COD_DISCIPLINA, DISCIPLINA):
+        self.URE = URE
+        self.USE = USE
+        self.MUNICIPIO = MUNICIPIO
+        self.COD_ESCOLA = COD_ESCOLA
+        self.COD_MEC = COD_MEC
+        self.COD_SETOR = COD_SETOR
+        self.ESCOLA_MATRICULA = ESCOLA_MATRICULA
+        self.VINCULO = VINCULO
+        self.SERVIDOR = SERVIDOR
+        self.COD_DISCIPLINA = COD_DISCIPLINA
+        self.DISCIPLINA = DISCIPLINA
 
-# Salvar as linhas filtradas em uma nova planilha
-linhas_filtradas.to_excel(diretorio_atual+'planilha.xlsx', index=False)
+# Criação de um objeto com atributos correspondentes aos nomes das colunas
+objeto = Objeto('valor1', 'valor2', 'valor3', 1, 101, 'A', 100, 'vinculo1', 'servidor1', 11, 'matematica')
+
+# Preenchendo as colunas do DataFrame com os atributos do objeto
+professores_lotados = professores_lotados._append(objeto.__dict__, ignore_index=True)
+
+# Exibindo o DataFrame atualizado
+print(professores_lotados)
